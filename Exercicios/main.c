3159f5abb9b4ac2e6ba8c9100ed68d908af3338c
Matheus Leite <matheusjoselfm@gmail.com>
2018-10-23 00:53:18 -0300

    Ajustes para atividade

--- Exercicios/main.c
+++ Exercicios/main.c
@@ -1,44 +1,104 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <stdbool.h>
 
+//DECLARANDO FUNCAO COPIAR, TAMANHO, COMPARAR
+char * copiar(char *dst, char *src);
+int len(char *src);
+bool comparar(char *dst, char *src);
+char * concatenar(char *dst, const char *src);
 int main()
 {
-    //Copiar String para uma variavel
-    char nome[7];
-    strcpy(nome, "Matheus");
-    printf("nome: %s \n", nome);
 
+    // INVOCANDO FUNCAO DE COPIAR STRING
+    char str1[] = "Python";
+    //char dst1[10];
+    char dst1[10];
+    printf("String copiada: %s\n", copiar(dst1, str1));
 
-    //Comparar duas strings
+    // INVOCANDO FUNCAO DE RETORNAR TAMANHO DE STRING
 
-    char src[4] = "mat";
-    char dst[4] = "mat";
-    int rst;
+    printf("Tamanho da String: %s e = %d \n",str1,len(str1));
 
-    rst = strcmp(src, dst);
-    if (rst == 0){
-        printf("'%s' igual a '%s'\n", src, dst);
-    }else if(rst < 0){
-        printf("'%s' menor que '%s'\n",src, dst);
-    }else if(rst > 0){
-        printf("'%s' maior que '%s'\n", src, dst);
-    }else
-    {
-        printf("Erro\n");
-    }
+    bool condition = comparar(str1, copiar(dst1, str1));
+    printf("Comparando '%s' e igual a '%s' ? R: %s \n",str1,copiar(dst1, str1), condition == 1?"VERDADEIRO":"FALSO" );
 
-    //Concatenar duas strings
-    char lang[12] = "Linguagem";
-    strcat(lang, " de Programacao C");
+    char nome[] = "MATHEUS";
+    char sobrenome[] = "LEITE";
 
-    printf("%s e melhor que java\n", lang);
-
-    //Tamanho da string
-    int tamanho;
-    tamanho = strlen(lang);
-    printf("Tamanho da string %s e: %i",lang, tamanho);
+    printf("Concatenando MATHEUS + LEITE = %s", concatenar(nome, sobrenome));
 
     return 0;
+}
+
+// METODO PARA COPIAR STRING
+char * copiar(char *dst, char *src) {
+    //VERIFICA SE FOI ALOCADO
+    if(dst == NULL && src == NULL){
+        return;
+    }
+
+    // ALOCA UMA VARIAVEL QUE IRA ARMAZENAR A NOVA STRING COM ENDERECO NA DE DESTINO
+    char *string = dst;
+
+
+    //Percorre toda string de src verificando ate o final dela
+    for (int x = 0; src[x] != '\0'; x++ ){
+        //Adiciona em dst posicao x o que tem em src na mesma posicao x
+        dst[x] = src[x];
+    }
+    //Retorna o valor do endereco de string
+    return string;
+}
+
+int len(char *src){
+	int tamanho = 0;
+
+	//For interando na string com condicao de parar o contador quando chegar no final da string
+	for (int x = 0; src[x] != '\0'; x++){
+        tamanho += 1;
+	}
+
+	return tamanho;
+}
+
+bool comparar(char *dst, char *src){
+
+
+	int resultado;
+
+    //Percorre as duas variaveis ate o final das duas em conjunto
+	for (int x = 0 ; dst[x] != '\0' && src[x] != '\0' ; x++){
+        //Se a posicao da primeira for igual a posicao da segunda tiverem os mesmos valores retorna true
+		if (dst[x] == src[x]){
+			return true;
+        //CASO nao retorna falso
+		}else{
+			return false;
+		}
+	}
+}
+
+char *concatenar(char *dst, const char *src){
+
+    int i,j;
+
+    //for para saber o tamanho da string destino
+    for (i = 0; dst[i] != '\0'; i++){
+
+    }
+
+    //segundo for para saber o tamanho da string origem
+    for (j = 0; src[j] != '\0'; j++){
+
+        //adiciona novas posições no array char de string com um valor da posicao da string de origem
+        dst[i+j] = src[j];
+    }
+
+    //Soma o tamanho das duas para adicionar o '\0' indicando fim da string
+    dst[i+j] = '\0';
+
+    return dst;
+
 }
 
